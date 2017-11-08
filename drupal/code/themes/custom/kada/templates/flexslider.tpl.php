36a3fa126832e8ca2510aa26a8774ccd107710bd
Tuukka Turu <tuukka.turu@druid.fi>
2017-11-08 14:53:07 +0200

    PORI-50: Class tweak

--- drupal/code/themes/custom/kada/templates/flexslider.tpl.php
+++ drupal/code/themes/custom/kada/templates/flexslider.tpl.php
@@ -7,7 +7,7 @@
 <div <?php print drupal_attributes($settings['attributes'])?>>
   <?php print theme('flexslider_list', array('items' => $items, 'settings' => $settings)); ?>
 
-  <div class="flex-caption-wrapper container flexslider__container">
+  <div class="flex-control-wrapper container flexslider__container">
     <div class="flexslider__controls flex-controls"></div>
   </div>
 </div>
