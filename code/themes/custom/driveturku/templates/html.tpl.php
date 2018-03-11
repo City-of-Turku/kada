af6da926f45be6f136de035c9a777a3996123d6b
back-2-95 <marko.korhonen@druid.fi>
2018-03-11 18:58:08 +0200

    Remove Qualaroo widget for Turku. Fixes #45

--- code/themes/custom/driveturku/templates/html.tpl.php
+++ code/themes/custom/driveturku/templates/html.tpl.php
@@ -60,16 +60,6 @@
   <?php print $scripts; ?>
 </head>
 <body<?php print $attributes;?>>
-  <!-- Qualaroo for turku.fi -->
-  <script type="text/javascript">
-    var _kiq = _kiq || [];
-    (function(){
-      setTimeout(function(){
-      var d = document, f = d.getElementsByTagName('script')[0], s = d.createElement('script'); s.type = 'text/javascript';
-      s.async = true; s.src = '//s3.amazonaws.com/ki.js/58607/d3v.js'; f.parentNode.insertBefore(s, f);
-      }, 1);
-    })();
-  </script>
   <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   <?php print $page_top; ?>
   <?php print $page; ?>
