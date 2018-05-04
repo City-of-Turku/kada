ad09b3459f5f58eaedff8b49badda612848e51c7
Tero Elonen <tero.elonen@druid.fi>
2018-05-04 19:41:09 +0300

    PORI-328 Added images to person card display

--- drupal/code/themes/custom/kada/ds_layouts/person_card_layout/person-card-layout.tpl.php
+++ drupal/code/themes/custom/kada/ds_layouts/person_card_layout/person-card-layout.tpl.php
@@ -4,13 +4,17 @@
  * @file
  * Person card layout template.
  */
+
+if (!empty($field_person_image)) {
+  $has_image = true;
+}
 ?>
 <?php if (isset($title_suffix['contextual_links'])): ?>
   <?php print render($title_suffix['contextual_links']); ?>
 <?php endif; ?>
 
 <div class="person-card">
-  <div class="person-card__inner">
+  <div class="person-card__inner<?php if (!empty($field_person_image )): ?> person-card__inner--has-image<?php endif; ?>">
     <?php print $main_content; ?>
   </div>
 </div>
