7b727a01a035b9e95bdcee99d41f1d2ee4df8bdd
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2018-05-14 10:53:50 +0300

    Working copy of pori waste search.

--- drupal/code/modules/features/pori_waste_search/js/chosen.js
+++ drupal/code/modules/features/pori_waste_search/js/chosen.js
@@ -4,10 +4,30 @@
       $(document).ready(function () {
         var chosen = '';
 
+        var map_view = $('#waste-search-map-ajax');
+        if (map_view.length !== 0) {
+          map_view.once('pori_waste_search_map', function() {
+            // Look through all the ajax views
+            for (var viewName in settings.views.ajaxViews) {
+              // Find the member_selection view
+              var view_selector = '.view-dom-id-' + settings.views.ajaxViews[viewName].view_dom_id;
+
+              // Refresh the view in a primitive sort of way.
+              if ($('#ip-geoloc-map-of-view-pori_waste_search_map-map').length === 1) {
+                if ($('#ip-geoloc-map-of-view-pori_waste_search_map-map>div').length === 0) {
+                  $(view_selector).triggerHandler('RefreshView');
+                }
+              }
+            }
+          });
+        }
+
         $('.chosen-select').once('pori_waste_search_label', function () {
-          // Chosenize each select list
+          // Chosenize each select list.
           chosen = $(this).chosen({
             width: '50%'
+          }).change(function(){
+            $('form.pori-waste-search-page-callback input[type=submit]').mousedown();
           });
         });
       });
