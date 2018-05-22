9ec3d678b0dfccef4187fc5e46251e722f67d406
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2018-05-22 13:45:11 +0300

    PORI-328: Fix coding style

--- drupal/code/themes/custom/kada/ds_layouts/person_card_layout/person-card-layout.tpl.php
+++ drupal/code/themes/custom/kada/ds_layouts/person_card_layout/person-card-layout.tpl.php
@@ -14,7 +14,7 @@ if (!empty($field_person_image)) {
 <?php endif; ?>
 
 <div class="person-card">
-  <div class="person-card__inner<?php if (!empty($field_person_image )): ?> person-card__inner--has-image<?php endif; ?>">
+  <div class="person-card__inner<?php if (!empty($field_person_image)): ?> person-card__inner--has-image<?php endif; ?>">
     <?php print $main_content; ?>
   </div>
 </div>
