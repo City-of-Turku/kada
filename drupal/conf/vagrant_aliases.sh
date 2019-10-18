780cad7a991ce049b8a761d550c099619180b7c6
Tormi Tabor <tormi.tabor@gmail.com>
2019-10-18 14:48:48 +0300

    Add vagrant aliases.

--- drupal/conf/vagrant_aliases.sh
+++ drupal/conf/vagrant_aliases.sh
@@ -2,7 +2,7 @@
 set -exu
 
 # Symlink the aliases file.
-rm -Rf $HOME/.drush
-mkdir -p $HOME/.drush
+rm -Rf /home/vagrant/.drush
+mkdir -p /home/vagrant/.drush
 ln -sfv /vagrant/drupal/drush/pori-kada.aliases.drushrc.php /home/vagrant/.drush/pori-kada.aliases.drushrc.php
 drush cc drush
