5e71506da83d1e794f8d804e139bd46a742f534f
Tormi Tabor <tormi.tabor@gmail.com>
2019-10-18 17:19:24 +0300

    Move aliases symlinks to site.yml.

--- drupal/conf/vagrant_aliases.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -exu
-
-# Symlink the aliases file.
-rm -Rf /home/vagrant/.drush
-mkdir -p /home/vagrant/.drush
-ln -sfv /vagrant/drupal/drush/pori-kada.aliases.drushrc.php /home/vagrant/.drush/pori-kada.aliases.drushrc.php
-drush cc drush
