9ffd83e810b4509d9a9387143743528cec3541cc
Olli Erinko <olli.erinko@wunder.io>
2019-12-12 12:42:36 +0200

    Updated simplesamlphp metadata and certs

--- drupal/conf/simplesaml/metadata/saml20-sp-remote.php
+++ drupal/conf/simplesaml/metadata/saml20-sp-remote.php
@@ -5,13 +5,18 @@
  * See: https://simplesamlphp.org/docs/stable/simplesamlphp-reference-sp-remote
  */
 
- $metadata['http://fs.pori.fi/adfs/services/trust'] = array (
-  'entityid' => 'http://fs.pori.fi/adfs/services/trust',
+$metadata['https://pori.dev.wunder.io/simplesaml/module.php/saml/sp/metadata.php/default-sp'] = array (
+  'entityid' => 'https://pori.dev.wunder.io/simplesaml/module.php/saml/sp/metadata.php/default-sp',
   'contacts' => 
   array (
     0 => 
     array (
-      'contactType' => 'support',
+      'contactType' => 'technical',
+      'givenName' => 'Administrator',
+      'emailAddress' => 
+      array (
+        0 => 'simplesaml_admin@example.com',
+      ),
     ),
   ),
   'metadata-set' => 'saml20-sp-remote',
@@ -20,21 +25,26 @@
     0 => 
     array (
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-      'Location' => 'https://fs.pori.fi/adfs/ls/',
+      'Location' => 'https://pori.dev.wunder.io/simplesaml/module.php/saml/sp/saml2-acs.php/default-sp',
       'index' => 0,
-      'isDefault' => true,
     ),
     1 => 
     array (
-      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact',
-      'Location' => 'https://fs.pori.fi/adfs/ls/',
+      'Binding' => 'urn:oasis:names:tc:SAML:1.0:profiles:browser-post',
+      'Location' => 'https://pori.dev.wunder.io/simplesaml/module.php/saml/sp/saml1-acs.php/default-sp',
       'index' => 1,
     ),
     2 => 
     array (
-      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-      'Location' => 'https://fs.pori.fi/adfs/ls/',
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact',
+      'Location' => 'https://pori.dev.wunder.io/simplesaml/module.php/saml/sp/saml2-acs.php/default-sp',
       'index' => 2,
+    ),
+    3 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:1.0:profiles:artifact-01',
+      'Location' => 'https://pori.dev.wunder.io/simplesaml/module.php/saml/sp/saml1-acs.php/default-sp/artifact',
+      'index' => 3,
     ),
   ),
   'SingleLogoutService' => 
@@ -42,31 +52,25 @@
     0 => 
     array (
       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
-      'Location' => 'https://fs.pori.fi/adfs/ls/',
-    ),
-    1 => 
-    array (
-      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-      'Location' => 'https://fs.pori.fi/adfs/ls/',
+      'Location' => 'https://pori.dev.wunder.io/simplesaml/module.php/saml/sp/saml2-logout.php/default-sp',
     ),
   ),
-  'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
   'keys' => 
   array (
     0 => 
     array (
-      'encryption' => true,
-      'signing' => false,
-      'type' => 'X509Certificate',
-      'X509Certificate' => 'MIIC1jCCAb6gAwIBAgIQTB4KjSl+TJpKYnrAfjhHEDANBgkqhkiG9w0BAQsFADAnMSUwIwYDVQQDExxBREZTIEVuY3J5cHRpb24gLSBmcy5wb3JpLmZpMB4XDTE4MTIwMjAwMTI1NVoXDTE5MTIwMjAwMTI1NVowJzElMCMGA1UEAxMcQURGUyBFbmNyeXB0aW9uIC0gZnMucG9yaS5maTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKqSF4e1tJJ21t7oMjIN67tuhogtsoc1YeBj3YYLKR258pbk7Ai1cqZesEEai3KZjR0wgwZpqWXOMPNmzEOdxabdwqJ8pbV7gdTWA0aVdBRzqTKJUlqxVLlmx6UAUW4M8J1/sd4cDVCw5Dc3oQ4eG4w48kIPSCVrvvQ7zhO8b1b7y1qVd20l3bu8iWHg5ImrKa7D52ltuROsLtq4ARljJAF5yenKXIjHoZxhTrw6yckcbSzSkcJ6T9Q/8Z/cndZOWXpreE/u5MqT0HrsYCZ6xkrPhHON4a6pcWPU2o11jjcQx0ioVqlmR5Zg8n5lRddSH8/XrU+T9MgU090TT7Rd/lUCAwEAATANBgkqhkiG9w0BAQsFAAOCAQEAjqKXcPlF3s/3MgEbtgcjUB17BCN+v19HGrWY5SdUO+8GmbAIiruglncXmuwDQK4JdACB4yb7AnxfkkTseT+yMw3Xi5S2ftfy0063RyVgeyX0zwvLYc2OECtmfdZKGU3d1gTJXaQ//T3UgqKJaNF8lwKSP1wH00CL5yVbuOzFgP8Lg62fb6TRSBuDrKr1UzRgiMl53E5O6BkQN5oIlrNVO6zTYMZDqAg9qw1l4/enYHP6gs9E5dQ49PQrm1S05rvF2GMmqLCQnrWIPQ9moZDFV/Kq47e72PWQF1POc+xJkH35DH3gHg6M5SLy5D5kHDNvrsNIq9SGeBDcXSPaqbR22A==',
-    ),
-    1 => 
-    array (
       'encryption' => false,
       'signing' => true,
       'type' => 'X509Certificate',
-      'X509Certificate' => 'MIIC0DCCAbigAwIBAgIQWjxVtLHc85xIP3OCew1WqjANBgkqhkiG9w0BAQsFADAkMSIwIAYDVQQDExlBREZTIFNpZ25pbmcgLSBmcy5wb3JpLmZpMB4XDTE4MTIwMjAwMTI1NloXDTE5MTIwMjAwMTI1NlowJDEiMCAGA1UEAxMZQURGUyBTaWduaW5nIC0gZnMucG9yaS5maTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBANLchyDHgXMvp95Q30+5QsPch2LADj55BTvg8sxfzogDCaq3oxHy8M1+cq5ZDrEpadYpg7BJmsSL+qsCS6xp6LF99JS2yrpZ3yiTl0HJgyeFj2VAia8QdwTakDfyYxORn8Hw7NIjOx+1IS1ju9gWDsiFJlxky4Gmw1PW+bXnGlosCgbPnNvU/l86DjX77EI38mri4dNSk6WL4fMgkF0PUykDhSktqGsSObUEVSnQH0AELmoWsjwocQ2PmGPH7KXYEaUdP2ngAaaiKcnCCza0nbf984zpJIzPkuHfLLoK73ov1CgbSi1QI2ZR8NBFn6nFHiaEmR8IFLBY3AycpYP88ZkCAwEAATANBgkqhkiG9w0BAQsFAAOCAQEAKWQy/pmrFdloV6v3tXOjDG9ujHBhXWN74ptLY5qz5Iww1ejwS8nQYTKgerKTZuOz6I1LcHWmQ3/bT3hNWA+25OWFGd+Yc7KM2j4342deaD0K9vDPISN3TCLhX6zOMviZqKB0Nl6GATwm7PFvGAM2awWPzSmUGCyllisn66VVkuHEU6Y+Qja+HXmsipefMvDHZk894ZDaN1X57AqKlAyJpq8Yu6QNqo2H4YgCF9KPb/vIGKRdlGwg8e0aTvumJo0z5HcNNaqFnGvcanE0VqPLo02mWuTB6JYSPr0vOS21gsF87NxQxIjUlYB383wW6y8SEG78wq0UNOp/+L4uFH3umg==',
+      'X509Certificate' => 'MIIETTCCArWgAwIBAgIJAJJyI2fNvUGNMA0GCSqGSIb3DQEBCwUAMD0xCzAJBgNVBAYTAkZJMQ0wCwYDVQQHDARQb3JpMQ0wCwYDVQQKDARQb3JpMRAwDgYDVQQDDAdwb3JpLmZpMB4XDTE5MTIxMDA2NTEyM1oXDTI5MTIwOTA2NTEyM1owPTELMAkGA1UEBhMCRkkxDTALBgNVBAcMBFBvcmkxDTALBgNVBAoMBFBvcmkxEDAOBgNVBAMMB3BvcmkuZmkwggGiMA0GCSqGSIb3DQEBAQUAA4IBjwAwggGKAoIBgQDU244aaYCBGggzacZtq0WIYIHZ/zJtO3iqmkvkxc6Yc0R4kgM+7v2OWvM5zs6f5c6Gh8ncxOkjwK2WMQ1OwO+zfE59YhcoauOgnscT1aPYkDp7yXv5o+sfwyp28X4S2y6qWQhY7djFaWKnqxWJ54iCyfoZdt9cRdueQpM44aA9wxlKAqO8MVceMvJ3czBbxK0Br25CIJLnw2VH5TJ7/NKkpMebp0Vy2ABSuPk32Ci6tjSboBsoVd5rNrZPh7yiJd5ICw5V0Jw9XHuUvKS7nBWyEAJgWEDHW3bK98zC6t0DVMPcjNtwFOm8+Nt/jkmHyFR3rLk8EO0w6Y7IX0JJ8+XjgD/kG3IdDviiT/Slqqxk9atKXpbzW2VXCCS1uqfgBtL9LPsAWcc+aMXWHjlEPPeqxBfjlE6VitZ/w2MZkZUL35cnKVZ/SD4QJOBWHQPhWTOlUEXPc0gwQD9InYZE/y7C/ZGUY4fGzvi4hSGWwCH1tRwDCkk3lPTY5cNgK9vzUWcCAwEAAaNQME4wHQYDVR0OBBYEFNoH9bEXzVz0OmD/qFTw54tiBt9BMB8GA1UdIwQYMBaAFNoH9bEXzVz0OmD/qFTw54tiBt9BMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQELBQADggGBAFaZq8UwUwQhuJNSKq6ZfqS/jF18sVvO7VIsxfwsJ7e3SsClq9W3b2w6a6eDxDx63Gn4wazglJI+T6Ux5sQuUEE9am9z0C24258f98lGul6sby9Jy3S9ebtJhOWnlTWb8hJOo48HARpkMSONvvU/zzofDkLjzHOy/lCsr0vaQqiZZEPA+hewoTF7JepvsnM4fSlejmcvdUKYeFdEx+S/3oF33VOdR3G6xgmY2ESk5QKsJdwf/HMbcSyEgBwFP3lGCVs6ucTbzQbqs5JXFXvgUUmZPrFOuKRwx76NQIAYfp6FRKBA2ze1rPJ5AexghNKQqXlOszK8NCj6unxtDglnMBZfd781XTrmgtwEnsaMd/6+H0Mve25dEzGSb0i7bgJ6vTvGUUB/i8Q94MppFGPV0Mbl8c052dZOE/LFYUXwLLmlnqU3hJ8dGeudJZQIcXhKGofmFAIHFxLamcbD+M8LSSFarjhEmCU7XlvhCK9OTk9MevLyFVvgBkIv7R5bQNErQA==',
+    ),
+    1 => 
+    array (
+      'encryption' => true,
+      'signing' => false,
+      'type' => 'X509Certificate',
+      'X509Certificate' => 'MIIETTCCArWgAwIBAgIJAJJyI2fNvUGNMA0GCSqGSIb3DQEBCwUAMD0xCzAJBgNVBAYTAkZJMQ0wCwYDVQQHDARQb3JpMQ0wCwYDVQQKDARQb3JpMRAwDgYDVQQDDAdwb3JpLmZpMB4XDTE5MTIxMDA2NTEyM1oXDTI5MTIwOTA2NTEyM1owPTELMAkGA1UEBhMCRkkxDTALBgNVBAcMBFBvcmkxDTALBgNVBAoMBFBvcmkxEDAOBgNVBAMMB3BvcmkuZmkwggGiMA0GCSqGSIb3DQEBAQUAA4IBjwAwggGKAoIBgQDU244aaYCBGggzacZtq0WIYIHZ/zJtO3iqmkvkxc6Yc0R4kgM+7v2OWvM5zs6f5c6Gh8ncxOkjwK2WMQ1OwO+zfE59YhcoauOgnscT1aPYkDp7yXv5o+sfwyp28X4S2y6qWQhY7djFaWKnqxWJ54iCyfoZdt9cRdueQpM44aA9wxlKAqO8MVceMvJ3czBbxK0Br25CIJLnw2VH5TJ7/NKkpMebp0Vy2ABSuPk32Ci6tjSboBsoVd5rNrZPh7yiJd5ICw5V0Jw9XHuUvKS7nBWyEAJgWEDHW3bK98zC6t0DVMPcjNtwFOm8+Nt/jkmHyFR3rLk8EO0w6Y7IX0JJ8+XjgD/kG3IdDviiT/Slqqxk9atKXpbzW2VXCCS1uqfgBtL9LPsAWcc+aMXWHjlEPPeqxBfjlE6VitZ/w2MZkZUL35cnKVZ/SD4QJOBWHQPhWTOlUEXPc0gwQD9InYZE/y7C/ZGUY4fGzvi4hSGWwCH1tRwDCkk3lPTY5cNgK9vzUWcCAwEAAaNQME4wHQYDVR0OBBYEFNoH9bEXzVz0OmD/qFTw54tiBt9BMB8GA1UdIwQYMBaAFNoH9bEXzVz0OmD/qFTw54tiBt9BMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQELBQADggGBAFaZq8UwUwQhuJNSKq6ZfqS/jF18sVvO7VIsxfwsJ7e3SsClq9W3b2w6a6eDxDx63Gn4wazglJI+T6Ux5sQuUEE9am9z0C24258f98lGul6sby9Jy3S9ebtJhOWnlTWb8hJOo48HARpkMSONvvU/zzofDkLjzHOy/lCsr0vaQqiZZEPA+hewoTF7JepvsnM4fSlejmcvdUKYeFdEx+S/3oF33VOdR3G6xgmY2ESk5QKsJdwf/HMbcSyEgBwFP3lGCVs6ucTbzQbqs5JXFXvgUUmZPrFOuKRwx76NQIAYfp6FRKBA2ze1rPJ5AexghNKQqXlOszK8NCj6unxtDglnMBZfd781XTrmgtwEnsaMd/6+H0Mve25dEzGSb0i7bgJ6vTvGUUB/i8Q94MppFGPV0Mbl8c052dZOE/LFYUXwLLmlnqU3hJ8dGeudJZQIcXhKGofmFAIHFxLamcbD+M8LSSFarjhEmCU7XlvhCK9OTk9MevLyFVvgBkIv7R5bQNErQA==',
     ),
   ),
-  'saml20.sign.assertion' => true,
+  'validate.authnrequest' => true,
 );
