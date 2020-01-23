22591c3e96e064a888fcf8a55c49bf5a1ae4e616
Tormi Tabor <tormi.tabor@gmail.com>
2020-01-23 11:01:42 +0200

    Remove dumpfile before lando syncdb task.

--- drupal/.lando/syncdb.sh
+++ drupal/.lando/syncdb.sh
@@ -31,7 +31,8 @@ fi
 
 # Synchronize & sanitize database from selected environment.
 cd "$webroot"
-drush "$site.$env" sql-dump --structure-tables-list=cache,cache_*,history,sessions,watchdog > /app/dump.sql
+rm -rf /app/dump.sql
+drush "$site.$env" sql-dump --structure-tables-list="cache,cache_*,history,sessions,watchdog" > /app/dump.sql
 drush "$local" sql-drop -y
 drush "$local" sql-query --file=/app/dump.sql -y
 drush "$local" sqlsan -y
